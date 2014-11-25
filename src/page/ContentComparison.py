cddc950674f428ba5f3c9a29f21917824e35a1a1
Alok Hota <ahota@vols.utk.edu>
2014-11-25 17:58:21 -0500

    Added refine_cluster to cull the articles included in our cluster and to define events based on important words

--- src/page/ContentComparison.py
+++ src/page/ContentComparison.py
@@ -16,3 +16,17 @@ def getWordCount(titles):
             pass
 
     return counter
+
+def compareWordCount(title1, title2):
+    stops = stopwords.words('english') + [u'==', u'===', u'']
+    seed_counter = Counter([word for word in iter(re.split('\s', wikipedia.page(title1).content.lower()))
+        if word not in stops]).most_common(10)
+    comp_counter = Counter([word for word in iter(re.split('\s', wikipedia.page(title2).content.lower()))
+        if word not in stops]).most_common(10)
+    results = []
+    for wordcount in seed_counter:
+        word = wordcount[0]
+        for wordcount2 in comp_counter:
+            if wordcount2[0] == word:
+                results.append(word)
+    return results
--- src/page/ReferenceComparison.py
+++ src/page/ReferenceComparison.py
@@ -2,24 +2,34 @@ import wikipedia
 import re
 import operator
 
-def getCommonRefs(title1, title2):
-    # retrieve pages
-    page1 = wikipedia.page(title1)
-    page2 = wikipedia.page(title2)
 
-    # get lists of reference urls
-    refs1 = page1.references
-    refs2 = page2.references
+# Changed input to page1 and title2 so that we don't constantly retrive the
+# same page when called from refine_cluster.py
+def getCommonRefs(page1, title2):
+
+    try:
+        # retrieve pages
+        #page1 = wikipedia.page(title1)
+        page2 = wikipedia.page(title2)
+
+        # get lists of reference urls
+        refs1 = page1.references
+        refs2 = page2.references
+    except:
+        return 0
 
     # find mutual references
     i = 0
 
     # print "mutual references:"
+    commons = [r for r in refs1 if r in refs2]
+    i = len(commons)
+    '''
     commons = []
     for r in refs1:
         if r in refs2:
             commons.append(r)
             i = i + 1
-
-    return commons
+    '''
+    return i
 
